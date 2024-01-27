6c5d8b209e419188319735233e4da6b967eb7d95
Angelina Liapin <angelina.liapin@gmail.com>
2024-01-27 12:29:40 +0200

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+import crypto from "crypto";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileToHash = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+  const stream = fs.createReadStream(fileToHash);
+  stream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  stream.on("end", () => {
+    const calculatedHash = hash.digest("hex");
+    console.log("Hash:", calculatedHash);
+  });
+
+  stream.on("error", (err) => {
+    console.error("Error:", err);
+  });
 };
 
 await calculateHash();
