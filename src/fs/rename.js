06ed37f2ef1d44a06cdc8123abf8dda8cc7d7f36
Angelina Liapin <angelina.liapin@gmail.com>
2024-01-28 18:24:56 +0200

    feat: implement rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from "node:fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const curentFile = path.join(__dirname, "files", "wrongFilename.txt");
+const renamedFile = path.join(__dirname, "files", "properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.promises.access(curentFile, fs.constants.F_OK);
+    await fs.promises.rename(curentFile, renamedFile);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
