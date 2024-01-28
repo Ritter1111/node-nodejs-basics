c14c8cc85715fe5fb69def07fc8ced6cd154e122
Angelina Liapin <angelina.liapin@gmail.com>
2024-01-28 22:02:05 +0200

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
-};
-
-parseArgs();
+    const allArguments = process.argv.slice(2);
+    const a = allArguments.reduce((acc, item, indx, array) => {
+      if (indx % 2 === 0) {
+        const filtered = item.slice(2);
+        const value = array[indx + 1];
+        const comma = indx < filtered.length - 1 ? "," : "";
+        acc.push(`${filtered} is ${value}${comma}`);
+      }
+      return acc;
+    }, []);
+    console.log(a.join(" "));
+  };
+  
+  parseArgs();
+  
