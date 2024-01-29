a5b4522b526854dfb763ca196057b3a15f306dc0
Angelina Liapin <angelina.liapin@gmail.com>
2024-01-29 12:16:12 +0200

    feat: implement zlib decompress.js task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import {  createGunzip } from 'node:zlib';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const curentFile = path.resolve(__dirname, "files", "fileToCompress.txt");
+const compressedFile = path.resolve(__dirname, "files", "archive.gz");
+
+const gzip = createGunzip();
+const source = fs.createReadStream(compressedFile);
+const destination = fs.createWriteStream(curentFile);
+
 const decompress = async () => {
-    // Write your code here 
+    source.pipe(gzip).pipe(destination)
+    destination.on('finish', () => {
+        console.log('Compressed finish');
+    });
+
+    destination.on('error', (e) => {
+        console.log(e);
+    })
 };
 
 await decompress();
